2988eaa543c46d113d03bf77cc0c41606e898e62
EPIC448 <samrey2018@gmail.com>
2021-12-13 11:15:13 -0500

    Python extra resourcs added

---
+++ Python Tools/iteration_python.py
@@ -0,0 +1,26 @@
+#  Resources used.: https://www.geeksforgeeks.org/iterate-over-a-list-in-python/?ref=lbp
+'''  Working
+string = "Hello World!"
+#  Iterate over a string in python
+# for element in string:
+#     print(element)
+
+for element in range(0, len(string)):
+    print(string[element])
+    
+'''
+
+'''
+#  Iterate over a list
+working
+
+array_of_list = [3,2,1,21]
+length = len(array_of_list)
+print(length) #4
+
+# for i in array_of_list:
+#     print(i)
+    
+for i in range(length):
+    print(array_of_list[i])
+'''

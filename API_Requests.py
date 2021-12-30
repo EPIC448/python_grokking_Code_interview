1f134fe0621b36e38e11dec24d04a948663d18ee
EPIC448 <samrey2018@gmail.com>
2021-12-30 14:08:10 -0500

    How we can query data and pull out what we want with parameter

--- API_Requests.py
+++ API_Requests.py
@@ -3,10 +3,12 @@ import requests # ignore the Line. this works now.
 import json
 
 #  Resources  https://www.dataquest.io/blog/python-api-tutorial/
+# https://www.nylas.com/blog/use-python-requests-module-rest-apis/   how to query API properly.
 #  ran command:  npm install requests
 
-
-response = requests.get("https://jsonplaceholder.typicode.com/comments")
+query ={"postId": 5 } # How to add a parameter into your query. very important.
+response = requests.get("https://jsonplaceholder.typicode.com/comments",params= query)
+# here, there may be some Iteration happening
 print(response.json())
 
 

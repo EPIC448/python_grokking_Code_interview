d153ec3245fe41b149f847bdee3b3b1f8a3d1dce
EPIC448 <samrey2018@gmail.com>
2022-01-05 11:59:39 -0500

    api relocated and smallest Subarry relocated.

--- sliding_window/smallest subarray with a given sum (easy).py
+++ sliding_window/smallest subarray with a given sum (easy).py
@@ -1,6 +1,6 @@
 '''
 Problem Statement 
-Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+Given an array of positive numbers and a positive number ‘S’, find the length of the smallest (contiguous) subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
 Example 1:
 Input: [2, 1, 5, 2, 3, 2], S=7 
 Output: 2

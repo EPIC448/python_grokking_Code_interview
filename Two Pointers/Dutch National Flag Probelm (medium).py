b3121ef90fe62a72970b5d624e347602beabfc91
EPIC448 <samrey2018@gmail.com>
2022-01-24 14:12:19 -0500

    add a new file for Lists, tuples, sets and updated python method and Dutch National Flag sit

--- Two Pointers/Dutch National Flag Probelm (medium).py
+++ Two Pointers/Dutch National Flag Probelm (medium).py
@@ -3,7 +3,7 @@ Problem Statement
 Given an array containing 0s, 1s and 2s, sort the array in-place. You should treat numbers of the array as objects,
 hence, we can’t count 0s, 1s, and 2s to recreate the array.
 The flag of the Netherlands consists of three colors: red, white and blue;
-and since our input array also consists of three different numbers that is why it is called Dutch National Flag problem.
+and since our input array also consists of three different numbers that is why it is called Dutch National Flag problem ( Dutch flag has Red, White, blue).
 
 Example 1:
 Input: [1, 0, 2, 1, 0]
--- python Methods/python_methods.py
+++ python Methods/python_methods.py
@@ -1,3 +1,21 @@
 #  TO Capitliaze as camel case use 
 str = "captain on Deck, look up"
 print(str.title()) # Captain On Deck, Look Up
+
+'''
+ Filling a empty string
+ barfoo = ''
+barfoo += 'h'
+barfoo += 'i'
+print(barfoo)
+
+
+ what this mean in python
+ :-1 means: Start at the end (the minus does that for you), end when nothing's left and walk backwards by 1 means: Start at the end (the minus does that for you), end when nothing's left and walk backwards by 1
+ Also to Reveres a string
+
+str = john
+ str[::-1] #=> nhoj
+
+'''
+
